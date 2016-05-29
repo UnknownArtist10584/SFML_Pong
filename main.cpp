cd3af456a3150ddfd3673efa23a531cea84b1cc2
UnknownArtist10584 <varungoofygumball@gmail.com>
2016-05-29 20:04:28 +0530

    Restructured code for gamestate variable and added pause screen

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ using namespace std;
 
 int main()
 {
+    // Game state variable
+    // 0 = Title screen, 1 = Game, 2 = Paused, 3 = Game won
+    int game_state = 1;
+
     sf::RenderWindow window(sf::VideoMode(800 , 600 ), "Title");
     window.setFramerateLimit(60);
 
@@ -37,114 +41,152 @@ int main()
     {
         while(window.pollEvent(event))
         {
-            if (event.type == event.Closed)
+            if (event.type == event.Closed)                 // Close the SFML Window
             {
                 window.close();
             }
-        }
 
-
-        // Check for user input and move the paddles accordingly
-        // TODO: Add "isPlaying" variable to add pause functionality
-
-        if (sf::Keyboard::isKeyPressed(player1.up_control))
-        {
-            player1.MovePaddle(player1.up_control, window.getSize());
-        }
-
-        if (sf::Keyboard::isKeyPressed(player1.down_control))
-        {
-            player1.MovePaddle(player1.down_control, window.getSize());
-        }
-
-        if (sf::Keyboard::isKeyPressed(player2.up_control))
-        {
-            player2.MovePaddle(player2.up_control, window.getSize());
-        }
-
-
-        if (sf::Keyboard::isKeyPressed(player2.down_control))
-        {
-            player2.MovePaddle(player2.down_control, window.getSize());
-        }
-
-        // Check for collisions with the paddles
-        // TODO: Calculate the angle of incidence and reflection for the new ballAngle
-        // TODO: Check whether the paddle is moving to add spin
-        // TODO: Decide what happens if the ball hits the bottom or top face of the paddle
-
-        if (ball.DidCollide(player1))
-        {
-            // TODO: Add movement with left and right instead of angles
-            // TODO: Find out what "Taking enum type from pointer" means
-
-            ball.horizontal_direction = 1;
-        }
-
-        if (ball.DidCollide(player2))
-        {
-            ball.horizontal_direction = -1;
-
-        }
-
-        // Check for collisions with the walls
-
-        if (ball.DidCollide(top_wall))
-        {
-            if (ball.vertical_direction == 1)
+            if (event.key.code == sf::Keyboard::Escape)
             {
-                ball.vertical_direction = -1;
+                window.close();
             }
-            else
+
+            if (event.key.code == sf::Keyboard::P)
             {
-                ball.vertical_direction = 1;
+                // If game is playing, pause it and vice-versa
+                
+                if (game_state == 1)
+                {
+                    game_state = 2;
+                }
+
+            }
+
+            if (event.key.code == sf::Keyboard::Return)
+            {
+                game_state = 1;
             }
         }
 
-        if (ball.DidCollide(bottom_wall))
+        if (game_state == 1)
         {
-            if (ball.vertical_direction == 1)
+            // Play the game
+            // Check for user input and move the paddles accordingly
+            // TODO: Add "isPlaying" variable to add pause functionality
+
+            if (sf::Keyboard::isKeyPressed(player1.up_control))
             {
-                ball.vertical_direction = -1;
+                player1.MovePaddle(player1.up_control, window.getSize());
             }
-            else
+
+            if (sf::Keyboard::isKeyPressed(player1.down_control))
             {
-                ball.vertical_direction = 1;
+                player1.MovePaddle(player1.down_control, window.getSize());
             }
+
+            if (sf::Keyboard::isKeyPressed(player2.up_control))
+            {
+                player2.MovePaddle(player2.up_control, window.getSize());
+            }
+
+
+            if (sf::Keyboard::isKeyPressed(player2.down_control))
+            {
+                player2.MovePaddle(player2.down_control, window.getSize());
+            }
+
+            // Check for collisions with the paddles
+            // TODO: Calculate the angle of incidence and reflection for the new ballAngle
+            // TODO: Check whether the paddle is moving to add spin
+            // TODO: Decide what happens if the ball hits the bottom or top face of the paddle
+
+            if (ball.DidCollide(player1))
+            {
+                // TODO: Add movement with left and right instead of angles
+                // TODO: Find out what "Taking enum type from pointer" means
+
+                ball.horizontal_direction = 1;
+            }
+
+            if (ball.DidCollide(player2))
+            {
+                ball.horizontal_direction = -1;
+
+            }
+
+            // Check for collisions with the walls
+
+            if (ball.DidCollide(top_wall))
+            {
+                if (ball.vertical_direction == 1)
+                {
+                    ball.vertical_direction = -1;
+                }
+                else
+                {
+                    ball.vertical_direction = 1;
+                }
+            }
+
+            if (ball.DidCollide(bottom_wall))
+            {
+                if (ball.vertical_direction == 1)
+                {
+                    ball.vertical_direction = -1;
+                }
+                else
+                {
+                    ball.vertical_direction = 1;
+                }
+            }
+
+
+            // Check if the ball is out of bounds ( Goal scored )
+
+            if (ball.ball.getPosition().x < 0)      // Player 2 has scored
+            {
+                player2.score++;
+                ball.ResetBall(window.getSize());
+            }
+
+            if (ball.ball.getPosition().x > window.getSize().x)      // Player 1 has scored
+            {
+                player1.score++;
+                ball.ResetBall(window.getSize());
+            }
+
+            // Move the ball at each frame
+            ball.Move();
+
+            // Update the score
+            score.setString(
+                    boost::lexical_cast<string>(player1.score) + " | " + boost::lexical_cast<string>(player2.score));
+
+            // Draw all the objects to the screen
+
+            window.clear();
+            window.draw(player1.paddle);
+            window.draw(player2.paddle);
+            window.draw(top_wall.wall);
+            window.draw(bottom_wall.wall);
+            window.draw(score);
+            window.draw(ball.ball);
+            window.display();
         }
-        
-        
-        // Check if the ball is out of bounds ( Goal scored )
-        
-        if (ball.ball.getPosition().x < 0)      // Player 2 has scored
+
+        if (game_state  == 2)
         {
-            player2.score ++;
-            ball.ResetBall(window.getSize());
+            // Display pause screen
+            sf::Text pause_text;
+            pause_text.setFont(font);
+            pause_text.setString("PAUSED");
+            pause_text.setCharacterSize(30);
+            pause_text.setPosition(window.getSize().x/2 - pause_text.getGlobalBounds().width/2,
+                                   window.getSize().y/2 - pause_text.getGlobalBounds().height/2);
+
+            window.clear();
+            window.draw(pause_text);
+            window.display();
         }
-
-        if (ball.ball.getPosition().x > window.getSize().x)      // Player 1 has scored
-        {
-            player1.score ++;
-            ball.ResetBall(window.getSize());
-        }
-
-        // Move the ball at each frame
-        ball.Move();
-
-        // Update the score
-        score.setString(boost::lexical_cast<string>(player1.score) + " | " + boost::lexical_cast<string>(player2.score) );
-
-        // Draw all the objects to the screen
-
-        window.clear();
-        window.draw(player1.paddle);
-        window.draw(player2.paddle);
-        window.draw(top_wall.wall);
-        window.draw(bottom_wall.wall);
-        window.draw(score);
-        window.draw(ball.ball);
-        window.display();
-
     }
 }
-
